124f55875616f3aecca6bed0b70bbb69e685f1d8
Sunnerstai <laa.zoger@gmail.com>
2024-11-28 10:47:50 +0100

    poiuytrfds

--- MegaManProject/Assets/EnemyController.cs
+++ MegaManProject/Assets/EnemyController.cs
@@ -1,18 +1,28 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    
+    [SerializeField] float enemyDamage = 2f;
+    GameObject player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+         player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject player = GameObject.FindWithTag("Player");
         transform.position = Vector2.MoveTowards(transform.position, player.transform.position, 7f * Time.deltaTime);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+    //IEnumerator FollowPlayer()
+    //{
+
+    //}
 }
