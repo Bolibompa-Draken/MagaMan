852b6e395ccee2d4b8120f15259aceee823a9750
Hugo Larsson <hugo.larsson1@learnet.se>
2024-12-12 08:15:31 +0100

    jag vet inte ens

--- MegaManProject/Assets/Scenes/Hugo/Health.cs
+++ MegaManProject/Assets/Scenes/Hugo/Health.cs
@@ -1,36 +1,36 @@
-    using UnityEngine;
-    using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.UI;
 
-    public class Health : MonoBehaviour
+public class Health : MonoBehaviour
+{
+    [SerializeField] public int maxHealth = 10;
+    [SerializeField] public int currentHealth;
+    [SerializeField] public Slider healthSlider;
+
+    void Start()
     {
-        [SerializeField] public int maxHealth = 10;
-        [SerializeField] public int currentHealth;
-        [SerializeField] public Slider healthSlider;
-
-        void Start()
-        {
-            currentHealth = maxHealth;
-            healthSlider.maxValue = maxHealth;
-            healthSlider.value = currentHealth;
-        }
-        public void TakeDamage(int damage)
-        {
-            currentHealth -= damage;
-
-            if (currentHealth < 0) currentHealth = 0;
-
-            healthSlider.value = currentHealth;
-        }
-        public void Heal(int amount)
-        {
-            currentHealth += amount;
-
-            if (currentHealth > maxHealth) currentHealth = maxHealth;
-
-            healthSlider.value = currentHealth;
-        }
-        public int GetCurrentHealth()
-        {
-            return currentHealth;
-        }
+        currentHealth = maxHealth;
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = currentHealth;
     }
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth < 0) currentHealth = 0;
+
+        healthSlider.value = currentHealth;
+    }
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+
+        if (currentHealth > maxHealth) currentHealth = maxHealth;
+
+        healthSlider.value = currentHealth;
+    }
+    public int GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+}
