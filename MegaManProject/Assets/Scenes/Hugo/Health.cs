0bcd135bd1f55ca5ebc3569ef8e8abe5a793c46d
Hugo Larsson <hugo.larsson1@learnet.se>
2024-12-12 08:15:41 +0100

    Revert "jag vet inte ens"
    This reverts commit 852b6e395ccee2d4b8120f15259aceee823a9750.

--- MegaManProject/Assets/Scenes/Hugo/Health.cs
+++ MegaManProject/Assets/Scenes/Hugo/Health.cs
@@ -1,36 +1,36 @@
-using UnityEngine;
-using UnityEngine.UI;
+    using UnityEngine;
+    using UnityEngine.UI;
 
-public class Health : MonoBehaviour
-{
-    [SerializeField] public int maxHealth = 10;
-    [SerializeField] public int currentHealth;
-    [SerializeField] public Slider healthSlider;
-
-    void Start()
+    public class Health : MonoBehaviour
     {
-        currentHealth = maxHealth;
-        healthSlider.maxValue = maxHealth;
-        healthSlider.value = currentHealth;
-    }
-    public void TakeDamage(int damage)
-    {
-        currentHealth -= damage;
+        [SerializeField] public int maxHealth = 10;
+        [SerializeField] public int currentHealth;
+        [SerializeField] public Slider healthSlider;
 
-        if (currentHealth < 0) currentHealth = 0;
+        void Start()
+        {
+            currentHealth = maxHealth;
+            healthSlider.maxValue = maxHealth;
+            healthSlider.value = currentHealth;
+        }
+        public void TakeDamage(int damage)
+        {
+            currentHealth -= damage;
 
-        healthSlider.value = currentHealth;
-    }
-    public void Heal(int amount)
-    {
-        currentHealth += amount;
+            if (currentHealth < 0) currentHealth = 0;
 
-        if (currentHealth > maxHealth) currentHealth = maxHealth;
+            healthSlider.value = currentHealth;
+        }
+        public void Heal(int amount)
+        {
+            currentHealth += amount;
 
-        healthSlider.value = currentHealth;
+            if (currentHealth > maxHealth) currentHealth = maxHealth;
+
+            healthSlider.value = currentHealth;
+        }
+        public int GetCurrentHealth()
+        {
+            return currentHealth;
+        }
     }
-    public int GetCurrentHealth()
-    {
-        return currentHealth;
-    }
-}
