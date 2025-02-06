12f098446b2809604ef9f3fed429127933647654
Hugo Larsson <hugo.larsson1@learnet.se>
2025-02-06 09:06:29 +0100

    Wave Spawner

---
+++ MegaManProject/Assets/Scenes/Hugo/WaveSpawner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject enemy;
+    [SerializeField] private Transform[] spawnpoints;
+    [SerializeField] float spawninterval = 10f;
+    [SerializeField] private float spawnincreaserate = 1.2f;
+    private int enemiesperwave = 1;
+    private float nextwave;
+
+    private void Start()
+    {
+        nextwave = Time.time + spawninterval;
+        StartCoroutine(SpawnWave());
+    }
+
+    IEnumerator SpawnWave()
+    {
+        while (true)
+        {
+            if (Time.time >= nextwave)
+            {
+                StartCoroutine(SpawnEnemies());
+                nextwave = Time.time + spawninterval;
+                enemiesperwave = (int)(enemiesperwave * spawnincreaserate) + 1;
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        for (int i = 0; i < enemiesperwave; i++)
+        {
+            Transform spawnpoint = spawnpoints[i % spawnpoints.Length];
+            Instantiate(enemy, spawnpoint.position, Quaternion.identity);
+            yield return new WaitForSeconds(1f);
+        }
+
+
+    }
+}
